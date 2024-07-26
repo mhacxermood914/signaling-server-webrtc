47099788182cb3dd677d9b9d9dee9c3d9f5c91f9
mhacxermood914 <soultonew2@gmail.com>
2024-07-26 12:02:44 +0100

    feat:update

--- index.js
+++ index.js
@@ -6,119 +6,119 @@ const cors = require('cors');
 const PORT = 3000;
 
 const app = express();
-const socketIo = require('socket.io')
-const server = app.listen(process.env.PORT | PORT, () => {
-    console.log(`server is running on http://localhost:${process.env.PORT ||PORT}`)
-})
+// const socketIo = require('socket.io')
+// const server = app.listen(process.env.PORT | PORT, () => {
+//     console.log(`server is running on http://localhost:${process.env.PORT ||PORT}`)
+// })
 
 
-const io = socketIo(server);
+// const io = socketIo(server);
 
-let socketToRoom= {}
-let rooms = {};
-io.on("connection", socket => {
-    socket.on("join", data => {
+// let socketToRoom= {}
+// let rooms = {};
+// io.on("connection", socket => {
+//     socket.on("join", data => {
 
-        // let a new user join to the room
+//         // let a new user join to the room
 
-        const roomId = data.room
+//         const roomId = data.room
 
-        socket.join(roomId);
+//         socket.join(roomId);
 
-        socketToRoom[socket.id] = roomId;
-
-
-
-        // persist the new user in the room
-
-        if (rooms[roomId]) {
-
-            rooms[roomId].push({id: socket.id, name: data.name});
-
-        } else {
-
-            rooms[roomId] = [{id: socket.id, name: data.name}];
-
-        }
+//         socketToRoom[socket.id] = roomId;
 
 
 
-        // sends a list of joined users to a new user
+//         // persist the new user in the room
 
-        const new_user = rooms[data.room].pop() //.filter(user => user.id !== socket.id);
+//         if (rooms[roomId]) {
 
-        console.log('socketid',socket.id)
+//             rooms[roomId].push({id: socket.id, name: data.name});
 
-        // if(rooms[data.room].length == 1){
-        //     socket.emit("memberJoined", new_user)
-        // }else{
-            socket.broadcast.emit("memberJoined", new_user)
-        // }
-        // socket.emit("memberJoined", new_user)
+//         } else {
 
+//             rooms[roomId] = [{id: socket.id, name: data.name}];
 
-        // io.sockets.to(socket.id).emit("room_users", users);
-
-        // console.log("[joined] room:" + data.room + " name: " + data.name);
-
-    });
-
-    socket.on("offer", data => {
-
-        socket.broadcast.emit("getOffer", {
-            offer: data.offer,
-            memberId: data.name
-        });
-
-        console.log("get offer: " + socket.id);
-
-    });
+//         }
 
 
 
-    socket.on("answer", data => {
+//         // sends a list of joined users to a new user
 
-        socket.broadcast.emit("getAnswer", {
-            answer: data.answer,
-            memberId: data.name
-        });
+//         const new_user = rooms[data.room].pop() //.filter(user => user.id !== socket.id);
 
-        console.log("answer: " + socket.id);
+//         console.log('socketid',socket.id)
 
-    });
-
-
-
-    socket.on("candidate", candidate => {
-
-        socket.broadcast.emit("getCandidate", candidate);
-
-        console.log("candidate: " + socket.id);
-
-    });
-
-    socket.on("disconnect", () => {
-
-        const roomId = socketToRoom[socket.id];
-
-        let room = rooms[roomId];
-
-        if (room) {
-
-            room = room.filter(user => user.id !== socket.id);
-
-            rooms[roomId] = room;
-
-        }
-
-        socket.broadcast.to(room).emit("user_exit", {id: socket.id});
-
-        console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
-
-    });
+//         // if(rooms[data.room].length == 1){
+//         //     socket.emit("memberJoined", new_user)
+//         // }else{
+//             socket.broadcast.emit("memberJoined", new_user)
+//         // }
+//         // socket.emit("memberJoined", new_user)
 
 
-})
+//         // io.sockets.to(socket.id).emit("room_users", users);
+
+//         // console.log("[joined] room:" + data.room + " name: " + data.name);
+
+//     });
+
+//     socket.on("offer", data => {
+
+//         socket.broadcast.emit("getOffer", {
+//             offer: data.offer,
+//             memberId: data.name
+//         });
+
+//         console.log("get offer: " + socket.id);
+
+//     });
+
+
+
+//     socket.on("answer", data => {
+
+//         socket.broadcast.emit("getAnswer", {
+//             answer: data.answer,
+//             memberId: data.name
+//         });
+
+//         console.log("answer: " + socket.id);
+
+//     });
+
+
+
+//     socket.on("candidate", candidate => {
+
+//         socket.broadcast.emit("getCandidate", candidate);
+
+//         console.log("candidate: " + socket.id);
+
+//     });
+
+//     socket.on("disconnect", () => {
+
+//         const roomId = socketToRoom[socket.id];
+
+//         let room = rooms[roomId];
+
+//         if (room) {
+
+//             room = room.filter(user => user.id !== socket.id);
+
+//             rooms[roomId] = room;
+
+//         }
+
+//         socket.broadcast.to(room).emit("user_exit", {id: socket.id});
+
+//         console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
+
+//     });
+
+
+// })
 const corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200
@@ -131,6 +131,11 @@ app.get('/', (req, res) => {
     res.send('hello, word!');
 
 })
+
+app.listen(process.env.PORT | PORT, () => {
+    console.log(`server is running on http://localhost:${process.env.PORT ||PORT}`)
+})
+    
 
 
 
